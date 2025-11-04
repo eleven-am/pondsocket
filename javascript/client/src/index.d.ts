ca2bd7773abea36bfd6afeddb422b064853b7164
Roy OSSAI <royossai@yahoo.com>
2025-11-04 12:35:39 +0100

    feat: update exports to include PondClient alongside ChannelState

--- javascript/client/src/index.d.ts
+++ javascript/client/src/index.d.ts
@@ -1,5 +1,4 @@
 import { PondClient } from './dist';
 import { ChannelState } from '@eleven-am/pondsocket-common';
 
-export default PondClient;
-export { ChannelState };
+export { ChannelState, PondClient };
--- javascript/client/src/index.ts
+++ javascript/client/src/index.ts
@@ -3,5 +3,6 @@ import { ChannelState } from '@eleven-am/pondsocket-common';
 import { PondClient as BrowserClient } from './browser/client';
 import { PondClient as NodeClient } from './node/node';
 
-export default typeof window === 'undefined' ? NodeClient : BrowserClient;
-export { ChannelState };
+const PondClient = typeof window === 'undefined' ? NodeClient : BrowserClient;
+
+export { ChannelState, PondClient };
